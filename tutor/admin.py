41921a738bbbea26bf39f6bf91c5cd3c323e2511
grantal <AlexV348@gmail.com>
2017-05-22 16:07:27 -0700

    made admin nicer

--- tutor/admin.py
+++ tutor/admin.py
@@ -2,16 +2,17 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 
-from tutor.models import Student
+import tutor.models as models
 
 
 class StudentInline(admin.StackedInline):
     """
     Inline field for the student
     """
-    model = Student
+    model = models.Student
     can_delete = False
-    verbose_name_plural = 'student'
+    verbose_name = "Student Information"
+    verbose_name_plural = "Student Information"
 
 
 # Define a new User admin
@@ -22,3 +23,7 @@ class UserAdmin(BaseUserAdmin):
 # Re-register UserAdmin
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
+
+# Register the rest of the fields
+admin.site.register(models.Subject)
+admin.site.register(models.Course)
--- tutor/apps.py
+++ tutor/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class TutorConfig(AppConfig):
     name = 'tutor'
+    verbose_name = 'Courses'
