af4489eeb27865f6d3d85bb608e73c2a789f773c
grantal <AlexV348@gmail.com>
2017-05-22 15:03:55 -0700

    added tutor and course models

--- tutor/admin.py
+++ tutor/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+from tutor.models import Student
+
+
+class StudentInline(admin.StackedInline):
+    """
+    Inline field for the student
+    """
+    model = Student
+    can_delete = False
+    verbose_name_plural = 'student'
+
+
+# Define a new User admin
+class UserAdmin(BaseUserAdmin):
+    inlines = (StudentInline, )
+
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
--- tutor/models.py
+++ tutor/models.py
@@ -1,5 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Student(models.Model):
+    """
+    Profile model for additional info about students
+    Right now it just stores whether or not a user is a tutor
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    tutor = models.BooleanField(default=False)
 
 
+class Subject(models.Model):
+    """
+    Subject that course can be taught in (i.e. Math, Humanities)
+    """
+    abbreviation = models.CharField(max_length=4, help_text="3-4 letter abbreviation for a subject (i.e. HUM)")
+    name = models.CharField(max_length=50, help_text="full name of the subject (i.e. Humanities)")
+
+
+class Course(models.Model):
+    """
+    model for a single course that people can tutor in
+    """
+    subject = models.ForeignKey('Subject', on_delete=models.CASCADE,)
+    number = models.IntegerField()
+    title = models.CharField(max_length=100, help_text="(i.e. Ancient Mediterranean, or Intro to Economic Analysis)")
