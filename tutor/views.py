4b27ba00520714acf5c005e2c64087273a133d14
grantal <AlexV348@gmail.com>
2017-05-23 13:38:30 -0700

    started startstop page

--- tutor/views.py
+++ tutor/views.py
@@ -7,14 +7,18 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello, world. tutoring is about to begin. This shall be the home page of the mighty tutoring-app-thing.")
 
+
 def courses(request):
     return HttpResponse("This be the courses page.")
+
 
 def admin(request):
     return HttpResponse("This be the admin page.")
 
+
 def tutors(request):
     return HttpResponse("You're looking at online and offline tutors.")
 
+
 def startstop(request):
-    return HttpResponse("This be the page to start and stop a tutor's session.")
+    return render(request, 'tutor/startstop.html')
