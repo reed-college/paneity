a6184ea003acf576c0426799501c4c24c0dd2a1c
grantal <AlexV348@gmail.com>
2017-05-30 14:36:29 -0700

    refactored add_users

--- tutor/views/gapi.py
+++ tutor/views/gapi.py
@@ -3,19 +3,15 @@ gapi.py
 This is the file for the views that use the google api
 """
 import httplib2
-import re
-import os
-from base64 import urlsafe_b64encode
 from apiclient import discovery
 from oauth2client import client
 
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
 
-import tutor.models as models
+import tutor.util as util
 
 
 @login_required(login_url='/admin/login/')
@@ -56,53 +52,7 @@ def add_users(request):
     connections = results.get('connections', [])
 
     # Now we add the connections to the db as users
-    reedmail = re.compile('.*@reed\.edu$')  # regex that matches reed emails
-
-    # print out every reed email in the connections list
-    # and its related profile id
-    for con in connections:
-
-        emails = con.get("emailAddresses", [])
-        reed_addr = None
-        # get reed email
-        for email in emails:
-            addr = email.get("value", "")
-            if addr and reedmail.match(addr):
-                reed_addr = addr
-        # get profileid
-        sources = con.get("metadata", {}).get("sources", [])
-        profile_id = None
-        for source in sources:
-            if source.get("type") == "PROFILE":
-                profile_id = source.get("id")
-        # get first and last name
-        # this gets the first thing in the "names" list
-        first_name = con.get("names", [{}])[0].get("givenName")
-        last_name = con.get("names", [{}])[0].get("familyName")
-
-        if reed_addr and profile_id and first_name and last_name:
-            # @reed.edu is 9 characters
-            username = reed_addr[0:-9]
-            pw = str(urlsafe_b64encode(os.urandom(6)))[2:10]
-
-            # see if this user already exists
-            usr = User.objects.filter(email=reed_addr).first()
-            # I'm using .filter and then .first rather than .get
-            # because .get throws an error if the user does not
-            # exist while .first just returns None
-            if usr:
-                stu = models.Student.objects.get(user=usr)
-            else:
-                usr = User.objects.create_user(
-                    username,
-                    first_name=first_name,
-                    last_name=last_name,
-                    email=reed_addr,
-                    password=pw)
-                stu = models.Student.objects.create(user=usr)
-            stu.profile_id = profile_id
-            usr.save()
-            stu.save()
+    util.add_connections(connections)
 
     return HttpResponse("Users were added!")
 
