319f6dda825f5b2fd2ca442eb1758072c1f9ade6
grantal <AlexV348@gmail.com>
2017-06-02 11:48:57 -0700

    changed tutor path back to tutor/ (I'm sorry)

--- paneity/urls.py
+++ paneity/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^', include('tutor.urls', namespace='tutor')),
+    url(r'^tutor/', include('tutor.urls', namespace='tutor')),
+    url(r'^board/', include('board.urls', namespace='board')),
     url(r'^admin/', admin.site.urls),
 ]
