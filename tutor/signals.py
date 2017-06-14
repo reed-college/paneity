e16cc3e902676fcab37575f326993bd8d88df46f
grantal <AlexV348@gmail.com>
2017-06-14 15:18:01 -0700

    made the signal work

--- tutor/signals.py
+++ tutor/signals.py
@@ -3,7 +3,7 @@ File for receiver functions.
 You can read more about what those are here:
 https://docs.djangoproject.com/en/1.11/topics/signals/
 """
-from django.db.models.signals import post_delete
+from django.db.models.signals import post_delete, m2m_changed
 from django.dispatch import receiver
 import tutor.models as models
 
@@ -17,3 +17,17 @@ def delete_category(sender, **kwargs):
     sub = kwargs.get("instance")
     if sub.category:
         sub.category.delete()
+
+
+@receiver(m2m_changed, sender=models.Student.tutoring_classes.through)
+def make_tutor(sender, **kwargs):
+    """
+    If the student is tutoring a class, automatically
+    set the tutor field to True
+    """
+    stu = kwargs.get("instance")
+    act = kwargs.get("action")
+    pks = kwargs.get("pk_set")
+    if stu.tutor is False and act == "post_add" and pks:
+        stu.tutor = True
+        stu.save()
