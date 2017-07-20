b9e9aebc97bbb9c69f69ad832ee271dca9643e0e
grantal <AlexV348@gmail.com>
2017-07-20 15:16:27 -0700

    got rid of all errors in dialogs.js

--- tutor/static/js/tutorchat.js
+++ tutor/static/js/tutorchat.js
@@ -5,117 +5,115 @@
  */
 
 
-$(document).ready(function () {
+$(document).ready(() => {
     // tell user they have no messages
-    if ($("#old-message-div").html().replace(/\s/g,'') == ""){
-        $("#new-message-div").html($("#no-messages-template").html());
-    }
+  if ($('#old-message-div').html().replace(/\s/g, '') == '') {
+    $('#new-message-div').html($('#no-messages-template').html());
+  }
 
     // This will make it so that when you click on a row,
     // it takes you to that conversation
-    $(".clickable-row").click(function() {
-        window.open($(this).data("href"), '_blank');
-    });
+  $('.clickable-row').click(function () {
+    window.open($(this).data('href'), '_blank');
+  });
 
-    var websocket = null;
+  let websocket = null;
 
-    function setupChatWebSocket() {
-        websocket = new WebSocket(base_ws_server_path + session_key + "/");
+  function setupChatWebSocket() {
+    websocket = new WebSocket(`${base_ws_server_path + session_key}/`);
 
-        websocket.onopen = function (event) {
+    websocket.onopen = function (event) {
+      const onOnlineCheckPacket = JSON.stringify({
+        type: 'check-online',
+        session_key,
+      });
+      const onConnectPacket = JSON.stringify({
+        type: 'online',
+        session_key,
 
-            var onOnlineCheckPacket = JSON.stringify({
-                type: "check-online",
-                session_key: session_key,
-            });
-            var onConnectPacket = JSON.stringify({
-                type: "online",
-                session_key: session_key
+      });
 
-            });
-
-            console.log('connected, sending:', onConnectPacket);
-            websocket.send(onConnectPacket);
-            console.log('checking online opponents with:', onOnlineCheckPacket);
-            websocket.send(onOnlineCheckPacket);
-        };
-
-
-        window.onbeforeunload = function () {
-
-            var onClosePacket = JSON.stringify({
-                type: "offline",
-                session_key: session_key,
-            });
-            console.log('unloading, sending:', onClosePacket);
-            websocket.send(onClosePacket);
-            websocket.close();
-        };
+      console.log('connected, sending:', onConnectPacket);
+      websocket.send(onConnectPacket);
+      console.log('checking online opponents with:', onOnlineCheckPacket);
+      websocket.send(onOnlineCheckPacket);
+    };
 
 
-        websocket.onmessage = function (event) {
-            var packet;
+    window.onbeforeunload = function () {
+      const onClosePacket = JSON.stringify({
+        type: 'offline',
+        session_key,
+      });
+      console.log('unloading, sending:', onClosePacket);
+      websocket.send(onClosePacket);
+      websocket.close();
+    };
 
-            try {
-                packet = JSON.parse(event.data);
-                console.log(packet)
-            } catch (e) {
-                console.log(e);
-            }
 
-            if (packet.type == "new-message") {
-                var username = packet['sender_name'];
+    websocket.onmessage = function (event) {
+      let packet;
+
+      try {
+        packet = JSON.parse(event.data);
+        console.log(packet);
+      } catch (e) {
+        console.log(e);
+      }
+
+      if (packet.type == 'new-message') {
+        const username = packet.sender_name;
                 // remove no message div and any message div with the same username
-                $("#no-messages-element").remove()
-                $("#"+username+"-element").remove()
+        $('#no-messages-element').remove();
+        $(`#${username}-element`).remove();
                 // add new message to new message div
-                var newm = $("#new-message-template").html().replace(/\[username\]/g, username);
-                newm = newm.replace(/\[message\]/g, packet['message']);
-                newm = newm + $("#new-message-div").html();
-                $("#new-message-div").html(newm);
+        let newm = $('#new-message-template').html().replace(/\[username\]/g, username);
+        newm = newm.replace(/\[message\]/g, packet.message);
+        newm += $('#new-message-div').html();
+        $('#new-message-div').html(newm);
                 // need to add click function for new row
 
-                $("#"+username+"-element").click(function() {
-                    window.open($(this).data("href"), '_blank');
-                });
+        $(`#${username}-element`).click(function () {
+          window.open($(this).data('href'), '_blank');
+        });
 
 
-                isNewMessage = true;
-                flash();
-            }
-        }
-    }
+        isNewMessage = true;
+        flash();
+      }
+    };
+  }
 
-    setupChatWebSocket();
+  setupChatWebSocket();
 });
 
 function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 // flashes the title once there is a new message
 async function flash() {
-    var oldtitle = document.title;
-    while (isNewMessage) {
-        await sleep(1000);
-        document.title="New Message";
-        await sleep(1000);
-        document.title=oldtitle;
-    }
+  const oldtitle = document.title;
+  while (isNewMessage) {
+    await sleep(1000);
+    document.title = 'New Message';
+    await sleep(1000);
+    document.title = oldtitle;
+  }
 }
 
 // stop flashing once window is in focus
-$(window).on("blur focus", function(e) {
-    var prevType = $(this).data("prevType");
+$(window).on('blur focus', function (e) {
+  const prevType = $(this).data('prevType');
 
-    if (prevType != e.type) {   //  reduce double fire issues
-        switch (e.type) {
-            case "blur":
-                break;
-            case "focus":
-                isNewMessage = false;
-                break;
-        }
+  if (prevType != e.type) {   //  reduce double fire issues
+    switch (e.type) {
+      case 'blur':
+        break;
+      case 'focus':
+        isNewMessage = false;
+        break;
     }
-    $(this).data("prevType", e.type);
-})
+  }
+  $(this).data('prevType', e.type);
+});
