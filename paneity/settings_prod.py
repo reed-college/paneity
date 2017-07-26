e35f739d07129f535bc34311abe621389c03c63d
grantal <AlexV348@gmail.com>
2017-07-26 15:26:35 -0700

    got rid of the settings template because thats stupid

---
+++ paneity/settings_prod.py
@@ -0,0 +1,17 @@
+"""
+This file gets set as the default settings by paneity/wsgi_prod.py and
+thats how those settings go in to effect
+"""
+from paneity.settings import *
+
+with open('/etc/secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
+
+DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'paneity'
+    }
+}
