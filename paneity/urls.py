0d891b4cb57f0055986a773c7536e1f3420081fe
Marika Swanberg <marlswanb@reed.edu>
2017-08-07 09:03:18 -0700

    updated comments in urls.py to reflect the changes i made

--- paneity/urls.py
+++ paneity/urls.py
@@ -2,12 +2,13 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    # This redirects to the urlpatterns in tutor.urls
+    # This redirects to the urlpatterns in spirit.urls
+    # This makes the spirit forum our homepage
     url(r'^', include('spirit.urls')),
     # This page allows admins to create new users, new courses,
     # new subjects, etc
     url(r'^admin/', admin.site.urls),
-    # This redirects to spirit urls
+    # This redirects to tutoring urls
     url(r'^tutoring/', include('tutor.urls', namespace='tutor')),
     # This redirects to django private chat urls, and this doesn't
     # interfere with the first url()
