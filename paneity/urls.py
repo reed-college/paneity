c93540b3bc1107e5f33ff6e7b6b240ce44906410
Marika Swanberg <marlswanb@reed.edu>
2017-08-07 08:37:35 -0700

    changed the main urls.py and it seems to be working

--- paneity/urls.py
+++ paneity/urls.py
@@ -3,12 +3,12 @@ from django.contrib import admin
 
 urlpatterns = [
     # This redirects to the urlpatterns in tutor.urls
-    url(r'^', include('tutor.urls', namespace='tutor')),
+    url(r'^', include('spirit.urls')),
     # This page allows admins to create new users, new courses,
     # new subjects, etc
     url(r'^admin/', admin.site.urls),
     # This redirects to spirit urls
-    url(r'^forum/', include('spirit.urls')),
+    url(r'^tutoring/', include('tutor.urls', namespace='tutor')),
     # This redirects to django private chat urls, and this doesn't
     # interfere with the first url()
     # because django looks at them in order from first to last
